817799943b8e08db46794bc6b9a43298e3e84269
jonvicbarcenas <exchantpro@gmail.com>
2024-08-11 16:36:48 +0800

    1.5

--- scripts/cmds/gitpush.js
+++ scripts/cmds/gitpush.js
@@ -1,10 +1,12 @@
+const { exec } = require('child_process');
+
 module.exports = {
     config: {
         name: "gitpush",
-        version: "1.1",
+        version: "1.5",
         author: "JV Barcenas",
         countDown: 5,
-        role: 0,
+        role: 2,
         description: {
             vi: "",
             en: "Push the latest code to the git repository"
@@ -12,7 +14,7 @@ module.exports = {
         category: "utility",
         guide: {
             vi: "",
-            en: ""
+            en: "{prefix}gitpush 'commit message'\nExample:\n{prefix}gitpush 'Update the code'"
         }
     },
 
@@ -28,9 +30,34 @@ module.exports = {
     onStart: async function ({ api, args, message, event, threadsData, usersData, dashBoardData, globalData, threadModel, userModel, dashBoardModel, globalModel, role, commandName, getLang }) {
         try{
             const commitmes = args.join(' ');
-            await message.reply(commitmes);
+            if (!message) {
+                message.reply('Commit message is required.');
+                return;
+            }
+            await message.reply('Pushing the latest code to the git repository...');
+
+            gitPush(commitmes, message);
         }catch(e){
             console.log(e);
         }
     }
 };
+
+function gitPush(commitMessage, message) {
+    exec(`git add . && git commit -m "${commitMessage}" && git push origin main`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`);
+            message.reply(`Error: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            message.reply(`stderr: ${stderr}`);
+            console.error(`stderr: ${stderr}`);
+            return;
+        }
+
+        console.log(`stdout: ${stdout}`);
+        message.reply('Successfully pushed the latest code to the git repository.');
+    });
+}
