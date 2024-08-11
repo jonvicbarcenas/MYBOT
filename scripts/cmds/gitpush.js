fca576c2e527240a23b5d7c6c50efabe69f9fa14
jonvicbarcenas <exchantpro@gmail.com>
2024-08-11 16:03:55 +0800

    wewe

---
+++ scripts/cmds/gitpush.js
@@ -0,0 +1,36 @@
+module.exports = {
+    config: {
+        name: "gitpush",
+        version: "1.0",
+        author: "JV Barcenas",
+        countDown: 5,
+        role: 0,
+        description: {
+            vi: "",
+            en: "Push the latest code to the git repository"
+        },
+        category: "utility",
+        guide: {
+            vi: "",
+            en: ""
+        }
+    },
+
+    langs: {
+        vi: {
+            
+        },
+        en: {
+            
+        }
+    },
+
+    onStart: async function ({ api, args, message, event, threadsData, usersData, dashBoardData, globalData, threadModel, userModel, dashBoardModel, globalModel, role, commandName, getLang }) {
+        try{
+            const commitmes = event.body;
+            await message.reply(commitmes);
+        }catch(e){
+            console.log(e);
+        }
+    }
+};
