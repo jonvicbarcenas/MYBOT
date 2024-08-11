3474c04ed934dafd71c5a3703e283b05b5b0af9c
jonvicbarcenas <exchantpro@gmail.com>
2024-08-11 19:06:00 +0800

    i don want this anymore

--- scripts/cmds/reply.js
+++ scripts/cmds/reply.js
@@ -48,12 +48,23 @@ module.exports = {
             return;
         }
         
-        // Verify if args is defined and contains the message text
-        const messageReply = event.body || args.join(" ");
-        
-        // Ensure message.reply is correctly called
-        return message.reply({
-            body: messageReply
+        const echoMessage = args.join(" ");
+        message.reply({
+            body: echoMessage
+        }, (err, info) => {
+            if (err) {
+                console.error("Error sending message:", err);
+                return;
+            }
+            // Ensure global.GoatBot.onReply is initialized as a Map
+            if (!global.GoatBot.onReply) {
+                global.GoatBot.onReply = new Map();
+            }
+            global.GoatBot.onReply.set(info.messageID, {
+                commandName: this.config.name,
+                author: message.senderID,
+                messageID: info.messageID,
+            });
         });
     }
 };
