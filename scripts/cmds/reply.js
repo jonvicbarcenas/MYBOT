66d3b1d4d9568204439a4078fd1bd8a9ce82de8b
jonvicbarcenas <exchantpro@gmail.com>
2024-08-11 19:09:57 +0800

    res

--- scripts/cmds/reply.js
+++ scripts/cmds/reply.js
@@ -44,27 +44,9 @@ module.exports = {
 
     onReply: async function ({ Reply, api, args, message, event, threadsData, usersData, dashBoardData, globalData, threadModel, userModel, dashBoardModel, globalModel, role, commandName, getLang }) {
         const { messageID, author } = Reply;
-        if (author != event.senderID) {
-            return;
-        }
-        
-        const echoMessage = args.join(" ");
-        message.reply({
-            body: echoMessage
-        }, (err, info) => {
-            if (err) {
-                console.error("Error sending message:", err);
-                return;
-            }
-            // Ensure global.GoatBot.onReply is initialized as a Map
-            if (!global.GoatBot.onReply) {
-                global.GoatBot.onReply = new Map();
-            }
-            global.GoatBot.onReply.set(info.messageID, {
-                commandName: this.config.name,
-                author: message.senderID,
-                messageID: info.messageID,
-            });
+       
+        return message.reply({
+            body: `Reply from ${author} with messageID ${messageID}`
         });
     }
 };
